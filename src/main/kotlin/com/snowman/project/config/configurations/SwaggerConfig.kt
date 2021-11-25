0db7524c0e91b8b4b6df79515e9ba6434a33a831
ktj1997 <rlaxowns7916@gmail.com>
2021-11-25 14:06:22 +0900

    fix: Swagger Request Server URL Setting

--- src/main/kotlin/com/snowman/project/config/configurations/SwaggerConfig.kt
+++ src/main/kotlin/com/snowman/project/config/configurations/SwaggerConfig.kt
@@ -1,10 +1,15 @@
 package com.snowman.project.config.configurations
 
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.servers.Server
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
 import springfox.documentation.oas.annotations.EnableOpenApi
+import springfox.documentation.oas.web.OpenApiTransformationContext
+import springfox.documentation.oas.web.WebMvcOpenApiTransformationFilter
 import springfox.documentation.service.ApiKey
 import springfox.documentation.service.AuthorizationScope
 import springfox.documentation.service.SecurityReference
@@ -14,10 +19,14 @@ import springfox.documentation.spring.web.plugins.Docket
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.LocalTime
+import javax.servlet.http.HttpServletRequest
 
 @Configuration
 @EnableOpenApi
-class SwaggerConfig {
+class SwaggerConfig(
+    @Value("\${app.url}")
+    private val url: String
+) : WebMvcOpenApiTransformationFilter {
 
     @Bean
     fun autApi(): Docket {
@@ -43,5 +52,20 @@ class SwaggerConfig {
     private fun defaultAuth(): List<SecurityReference> {
         val authorizationScope = AuthorizationScope("global", "access All")
         return listOf(SecurityReference("Authorization", arrayOf(authorizationScope)))
+    }
+
+    override fun supports(delimiter: DocumentationType): Boolean {
+        return delimiter == DocumentationType.OAS_30;
+    }
+
+    override fun transform(context: OpenApiTransformationContext<HttpServletRequest>?): OpenAPI {
+        val openAPI = context!!.specification
+        val server = Server()
+
+        server.url = url
+        server.description = "snowe-api-server"
+
+        openAPI.servers(listOf(server))
+        return openAPI
     }
 }
