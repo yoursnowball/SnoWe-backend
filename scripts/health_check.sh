8a2db8a3ae33dba56673f7202adf03dc5789f094
ktj1997 <rlaxowns7916@gmail.com>
2021-12-20 01:38:33 +0900

    fix: health_check.sh

--- scripts/health_check.sh
+++ scripts/health_check.sh
@@ -3,7 +3,6 @@
 
 CURRENT_PORT=$(cat /etc/nginx/service_url.inc | grep -Po '[0-9]+' | tail -1)
 TARGET_PORT=0
-TARGET_URL="swagger-ui/index.html"
 
 if [ ${CURRENT_PORT} -eq 8081 ]; then
   TARGET_PORT=8082
@@ -15,7 +14,7 @@ fi
 for RETRY_COUNT in `seq 1 10`
 do
   echo "> #${RETRY_COUNT} trying..."
-  RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}"http://127.0.0.1:${TARGET_PORT}/${TARGET_URL})
+  RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://127.0.0.1:${TARGET_PORT}/"swagger-ui/index.html")
   if [ ${RESPONSE_CODE} -eq 200 ]; then echo "> New WAS successfully running"
   exit 0
   elif [ ${RETRY_COUNT} -eq 10 ]; then
